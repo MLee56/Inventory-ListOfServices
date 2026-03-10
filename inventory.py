c5619ef304af3fb58f1db523791896d1d82296e1
Yongli Guo <guo_lily@yahoo.com>
2026-03-09 22:11:33 -0700

    Fixed comments

--- inventory.py
+++ inventory.py
@@ -2,49 +2,38 @@ import time
 import os
 
 while True:
-    # view inventory
     if os.path.exists("view-request.txt"):
         with open("view-request.txt", "r+", encoding="utf-8") as f:
             name = f.read().strip()
-
         os.remove("view-request.txt")
-
         if name:
             if os.path.exists(f'{name}.txt'):
-
                 with open(f'{name}.txt', "r", encoding="utf-8") as f:
                     quantity = f.read().strip()
                     response = quantity
             else:
                 response = "Item does not exist"
-
             with open("view-response.txt", "w", encoding="utf-8") as f:
                 f.write(response)
 
-    # update inventory
     if os.path.exists("update-request.txt"):
         with open("update-request.txt", "r", encoding="utf-8") as f:
             name = f.readline().strip()
             newQuantity = f.readline().strip()
-
         os.remove("update-request.txt")
-
         if not name:
             response = "Item name is empty"
         elif not newQuantity.isnumeric():
             response = "Invalid quantity"
         elif os.path.exists(f'{name}.txt'):
-
             with open(f'{name}.txt', "w", encoding="utf-8") as f:
                 f.write(newQuantity)
             response = "Quantity updated successfully"
         else:
             response = "Item does not exist"
-
         with open("update-response.txt", "w", encoding="utf-8") as f:
             f.write(response)
 
-    # alert-request
     if os.path.exists("alert-request.txt"):
         with open("alert-request.txt", "r", encoding="utf-8") as f:
             name = f.readline().strip()
@@ -65,7 +54,6 @@ while True:
     if os.path.exists("alerts.txt"):
         with open("alerts.txt", "r", encoding="utf-8") as f:
             alerts = f.readlines()
-
         untriggered_alerts = []
         for alert in alerts:
             name, threshold = alert.strip().split(",")
@@ -74,16 +62,11 @@ while True:
                 continue
             with open(f'{name}.txt', "r", encoding="utf-8") as f:
                 quantity = f.read().strip()
-
             if int(quantity) < int(threshold):
                 with open("alert-notification.txt", "w", encoding="utf-8") as f:
                     f.write(f"Alert: {name} quantity is below {threshold}")
             else:
                 untriggered_alerts.append(alert)
-
         with open("alerts.txt", "w", encoding="utf-8") as f:
             f.writelines(untriggered_alerts)
-
     time.sleep(2)
-
-
